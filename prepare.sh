0df255c60bbd9c5a2414e6b757423b3f75ca9627
Michelle <181282244+muhchellee@users.noreply.github.com>
2024-12-04 20:07:33 -0800

    Fixed prepare.sh script

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,7 @@ python3 generate_dirty_data.py
 
 # step 2: cleaning dirty data
 # remove comment lines, empty lines, and empty commas; extract essential columns 
-grep -v '^#' ms_data_dirty.csv | sed '/^$/d' | sed -e 's/, ,/,/g' | cut -d ',' -f1,2,4,5,6 > ms_data.csv
+grep -v '^#' ms_data_dirty.csv | sed -e '/^$/d' | sed -e 's/, ,/,/g' | cut -d ',' -f 1,2,4,5,6 | awk -F ',' 'NR==1 || $5 >= 2.0 && $5 <= 8.0' > ms_data.csv
 
 # step 3: creating insurance file
 echo -e "insurance_type\nBronze\nSilver\nGold" > insurance.lst
